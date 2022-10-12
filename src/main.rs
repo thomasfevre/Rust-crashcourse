d827dc108b66f0593a56eb60e812a9a323e44e98
thomasfevre <thomas.fevre@viacesi.fr>
2022-10-12 09:37:44 +0200

    first test

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 fn main() {
     let square = Square::<u32>::new(5);
-    let square_float = Square::<f64>::new(5.4);
-    let square_string = Square::<String>::new("6");
+    // let square_float = Square::<f64>::new(5.4);
+    // let square_string = Square::<String>::new("6");
 
     println!("square area is {}", square.area());
-    println!("square_float area is {}", square_float.area());
-    println!("square_string area is {}", square_string.area());
+    // println!("square_float area is {}", square_float.area());
+    // println!("square_string area is {}", square_string.area());
 
-    let triangle = Triangle::new(14.9, 20.1);
-    let pyramid_square = Pyramid::<Square<u32>, f64>::new(square, 24.3);
-    let pyramid_triangle = Pyramid::<Triangle<f64>, f64>::new(triangle, 24.3);
+    // let triangle = Triangle::new(14.9, 20.1);
+    // let pyramid_square = Pyramid::<Square<u32>, f64>::new(square, 24.3);
+    // let pyramid_triangle = Pyramid::<Triangle<f64>, f64>::new(triangle, 24.3);
 
-    println!("pyramid_square volume is {}", pyramid_square.volume());
-    println!("pyramid_triangle volume is {}", pyramid_triangle.volume());
+    // println!("pyramid_square volume is {}", pyramid_square.volume());
+    // println!("pyramid_triangle volume is {}", pyramid_triangle.volume());
+}
+
+pub trait Form {
+    pub fn area();
+    pub fn volume();
+}
+
+struct Square{
+    edge_length: u32;
+}
+
+impl Form for Square {
+    pub fn area(&self){
+        let result = *self*self;
+        return result;
+    }
 }
